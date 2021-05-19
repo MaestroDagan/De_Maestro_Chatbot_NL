a768e49d74c68f532d97b03af0fa248853072d7d
MaestroDagan <ditisdagan@hotmail.com>
2021-05-19 19:28:36 +0200

    2.9st upload

--- index.js
+++ index.js
@@ -55,24 +55,4 @@ client.on("message", async message => {
     if(commands) commands.run(client, message, arguments)
 
 
-    if(command === `${prefix}commands`){
-//        
-//        var HelpEmbed = new testBot.MessageEmbed()
-//        .setTitle("Commands")
-//        .setColor("#ff1cbf")
-//        .addFields(
-//            {name: ".hallo", value: "Zeg hallo tegen de bot"}
-//            ,{name: ".hoeheetje", value: "Vraag de bot om zijn naam"}
-//            ,{name: ".hebjeouders", value: "Vraag de bot of hij ouders heeft"}
-//            ,{name: ".botmaker", value: "Vraag de bot wie hem heeft gemaakt"}
-//            ,{name: ".hoegaathet", value: "Vraag de bot hoe het met hem gaat"}
-//            ,{name: ".watwiljelaterworden", value: "Vraag de bot wat hij later wil worden"}
-//            ,{name: ".watishetstil", value: "Laat de bot deze ongemakkelijke situatie stoppen"}
-//            ,{name: ".doei", value: "Zeg doei tegen de bot"}
-//            ,{name: ".commands", value: "Krijg dit bericht opnieuw te zien"}
-//        );
-//        
-//        return message.channel.send(HelpEmbed);
-    }
-
 });
